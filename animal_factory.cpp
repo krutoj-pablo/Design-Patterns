0dbe6beb2c40fcb0992a211cb519f665f9c557fa
Pavel Ponomariov <pavelponomariov@gmail..com>
2012-11-04 21:12:57 +0100

    Added factory classes
    what happened
    
    [ticket: X]

---
+++ animal_factory.cpp
@@ -0,0 +1,40 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  animal_factory.cpp
+ *
+ *    Description:  Implementation of AnimalFactory class
+ *
+ *        Version:  1.0
+ *        Created:  11/04/2012 08:47:04 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Pavel Ponomariov (Pavel), pavelponomariov@gmail.com
+ *        Company:  Private Company
+ *
+ * =====================================================================================
+ */
+
+#include "tiger.h"
+#include "elephant.h"
+#include "human.h"
+
+AnimalFactory::AnimalFactory()
+{
+	m_animalsMap["tiger"] = new Tiger("Sher Khahn", 10);
+	m_animalsMap["elephant"] = new Elephant("King", 4);
+	m_animalsMap["human"] = new Human("Ricardo", 25);
+}
+
+AnimalFactory::~AnimalFactory()
+{
+	std::map<std::string, Animal*>::iterator iter = m_animalsMap.begin();
+	for(; iter != m_animalsMap.end(); iter++)
+		delete iter->second();
+}
+
+Animal* AnimalFactory::createAnimal(std::string name)
+{
+	return m_animalsMap[name]->clone();
+}
