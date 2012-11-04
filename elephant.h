18261dd400c9266a62ed895b424a8bb68d99e757
Pavel Ponomariov <pavelponomariov@gmail..com>
2012-11-04 20:28:25 +0100

    Added concrete implementation of abstract class animal

---
+++ elephant.h
@@ -0,0 +1,32 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  elephant.h
+ *
+ *    Description:  Declaration of Elephant class
+ *
+ *        Version:  1.0
+ *        Created:  11/04/2012 08:24:06 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Pavel Ponomariov (Pavel), pavelponomariov@gmail.com
+ *        Company:  Private Company
+ *
+ * =====================================================================================
+ */
+#ifndef ELEPHANT_H
+#define ELEPHANT_H
+
+#include "animal.h"
+
+class Elephant : public Animal
+{
+	public:
+		Elephant(Elephant &e);
+		virtual ~Elephant() {};
+		virtual Animal *clone();
+		virtual void print();
+};
+#endif
+
